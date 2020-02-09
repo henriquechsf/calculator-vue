23116ef2167faa995e9b48d9c5a60d14920ffd53
Carlos Henrique <ch.ferreir@hotmail.com>
2020-02-09 20:15:24 -0300

    implementa a lógica da calculadora

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -41,8 +41,30 @@ export default {
             //retorna ao estado inicial do componente
             Object.assign(this.$data, this.$options.data())
         },
-        setOperation() {
-            
+        setOperation(operation) {
+            if (this.current === "0") {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch(e) {
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
         },
         addDigit(n) {
             if (n === '.' && this.displayValue.includes(".")) {
