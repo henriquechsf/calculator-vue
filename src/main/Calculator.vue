6188b0e61f88e0d507ee40c46f3dae3074a8a1c8
Carlos Henrique <ch.ferreir@hotmail.com>
2020-02-09 20:00:10 -0300

    funcionamento dos botões da calculadora

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calculator">
-        <Display value="1000" />
+        <Display :value="displayValue" />
         <Button label="AC" triple @onClick="clearMemory" />
         <Button label="/" operation @onClick="setOperation" />
         <Button label="7" @onClick="addDigit" />
@@ -26,16 +26,42 @@ import Display from '../components/Display'
 import Button from '../components/Button'
 
 export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
     components: { Button, Display },
     methods: {
         clearMemory() {
-            
+            //retorna ao estado inicial do componente
+            Object.assign(this.$data, this.$options.data())
         },
-        setOperation(operation) {
+        setOperation() {
             
         },
         addDigit(n) {
-            
+            if (n === '.' && this.displayValue.includes(".")) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            //adiciona o primeiro valor no display (funcionamento dos botões)
+            if (n !== ".") {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
         }
     }
     
